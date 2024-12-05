1c58e16ae87b9e8ac1bff9d510088ed9b47f8e73
frederico nakajima <fredericonakajimanakajima@gmail.com>
2024-12-05 03:30:55 -0300

    .env

--- src/configs/auth.js
+++ src/configs/auth.js
@@ -1,6 +1,9 @@
 module.exports = {
     jwt: {
-        secret:"default",
+        secret: process.env.AUTH_SECRET || "default",
         expiresIn:"1d"
     }
 }
+
+
+const PORT = process.env.SERVER_PORT;
